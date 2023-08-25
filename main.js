214560e9d58c265f3169e26bba87ebd0aed3c79b
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-24 23:58:58 -0700

    add

---
+++ main.js
@@ -0,0 +1,33 @@
+//DEFAULT PARAMETER
+
+
+call = (a, b) => {
+	
+	console.log(a + b);
+	
+}
+
+call(10, 12);
+
+
+
+
+
+hello = (a=3, b=4) => {
+	
+	console.log(a + b);
+	
+}
+
+hello(10, 12);
+
+
+
+
+output = (a=3, b=4) => {
+	
+	console.log(a + b);
+	
+}
+
+output(10);
